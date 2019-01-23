8f2c7513126cbd2a8a73378f97136b9f60670440
Jack Leetch <Jack@jack-leetch.bc.edu>
2019-01-23 16:32:22 -0500

    Initial Commit

---
+++ Week 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week 2
+//
+//  Created by Jack Ryan Leetch on 1/23/19.
+//  Copyright © 2019 Jack Ryan Leetch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
